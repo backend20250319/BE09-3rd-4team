4db6de79fdf3bc577e69923379d66cee1a74f2f0
Berry-mas <junseo9145@gmail.com>
2025-06-21 01:39:49 +0900

    api 추가

--- movie-service/src/main/java/com/smile/movieservice/client/ReviewClient.java
+++ movie-service/src/main/java/com/smile/movieservice/client/ReviewClient.java
@@ -6,17 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// 1. Gateway 를 호출하는 상황
-// @FeignClient(name = "user-service", url = "http://localhost:8000", configuration = FeignClientConfig.class)
-// 2. 내부에서 user-service를 호출하는 상황
-@FeignClient(name = "smile-movie-service", configuration = FeignClientConfig.class)
+import java.util.List;
+
+@FeignClient(name = "review-service", path = "/internal/reviews", configuration = FeignClientConfig.class)
 public interface ReviewClient {
 
-    // User Service에서 사용자 상태나 간단한 정보를 조회하는 API
-    // 1. Gateway를 호출하는 상황
-//    @GetMapping("/api/v1/user-service/users/{userId}/grade")
+    @GetMapping("/movie/{movieId}/average-rating")
+    Double getAverageRatingByMovieId(@PathVariable("movieId") Long movieId);
 
-    // 2. 내부에서 user-service를 호출하는 상황
-    @GetMapping("/reviews/{reviewId}")
-    ApiResponse<Double> getMovieId(@PathVariable("movieId") Long movieId);
+    @GetMapping("/movie/{movieId}/ratings")
+    List<Double> getRatingsByMovieId(@PathVariable("movieId") Long movieId);
 }
+
